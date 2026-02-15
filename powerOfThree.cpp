3ee06540e6902602288c566926a3663f5018625b
Shyam4849 <piyush.soni8877@gmail.com>
2026-02-16 00:16:17 +0530

    LeetCode 326

--- powerOfThree.cpp
+++ powerOfThree.cpp
@@ -3,19 +3,20 @@ using namespace std;
 
 bool isPowerOfThree(int n)
 {
+    // Brute Force
     /*
-    if (n < 1)
-    return false;
+        if (n < 1)
+        return false;
 
-    for (int i = 0; i <= n; i++) {
-        if(n == pow(3, i)) return true;
+        for (int i = 0; i <= n; i++) {
+            if(n == pow(3, i)) return true;
 
-        if(n < pow(3, i)) return false;
-    }
-    return false;
+            if(n < pow(3, i)) return false;
+        }
+        return false;
     */
 
-    // Better Approach
+    // Better Approach - 1
     if (n <= 0)
         return false;
     if (n == 1)
@@ -29,6 +30,19 @@ bool isPowerOfThree(int n)
             return true;
     }
     return false;
+
+    // Better Approach - 2
+    /*
+            long long current = 1;
+            if(n==1) return true;
+
+            while(current < n){
+                current = current * 3;
+
+                if(current == n) return true;
+            }
+            return false;
+    */
 }
 
 int main()
