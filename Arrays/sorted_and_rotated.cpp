cfb0e3e39450bc2e843e776d31e0d87982d72319
Shyam4849 <piyush.soni8877@gmail.com>
2026-02-23 23:30:17 +0530

    LeetCode Problem 1752

---
+++ Arrays/sorted_and_rotated.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(vector<int> &nums)
+{
+    int n = nums.size();
+    int breakCount = 0;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (nums[i] > nums[i + 1])
+        {
+            breakCount++;
+        }
+    }
+
+    if (nums[n - 1] > nums[0])
+        breakCount++;
+
+    if (breakCount <= 1)
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 4, 5};
+
+    cout << check(nums);
+}
