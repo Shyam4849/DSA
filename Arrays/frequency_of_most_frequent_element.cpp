36ef75162d296df4972151161b9777ff271d242a
Shyam4849 <piyush.soni8877@gmail.com>
2026-03-07 10:39:49 +0530

    Organised

---
+++ Arrays/frequency_of_most_frequent_element.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxFrequency(vector<int> &nums, int k)
+{
+    sort(nums.begin(), nums.end());
+    int maxFreq = 1;
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        long long cost = 0;
+        int count = 1;
+        for (int j = i - 1; j >= 0; j--)
+        {
+            cost += nums[i] - nums[j];
+            if (cost > k)
+                break;
+            count++;
+        }
+        maxFreq = max(maxFreq, count);
+    }
+    return maxFreq;
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 4}; // {1,3,8,13} k = 5
+    int k = 5;
+
+    cout << maxFrequency(nums, k);
+}
