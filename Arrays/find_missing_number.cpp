26769636a8b7e20ac791b187588bec2397e134fe
Shyam4849 <piyush.soni8877@gmail.com>
2026-03-10 21:55:06 +0530

    A2Z Striver Sheet

--- Arrays/find_missing_number.cpp
+++ Arrays/find_missing_number.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int missingNum(vector<int> &nums)
 {
+    // Method 1 -> chances of overflow
+    /*
     int n = nums.size();
     int arrSum = 0;
 
@@ -15,6 +17,26 @@ int missingNum(vector<int> &nums)
     int sum = (realN * (realN + 1)) / 2;
 
     return sum - arrSum;
+    */
+
+    // Method 2 -> using XOR operator
+    int n = nums.size();
+    int xor1 = 0, xor2 = 0;
+
+    // XOR all array elements
+    for (int i = 0; i < n - 1; i++)
+    {
+        xor2 ^= nums[i];
+    }
+
+    // XOR all numbers from 1 to n
+    for (int i = 1; i <= n; i++)
+    {
+        xor1 ^= i;
+    }
+
+    // Missing number is the XOR of xor1 and xor2
+    return xor1 ^ xor2;
 }
 
 int main()
