0649430ea57be23ea0549d391e3df88a4ad5aa6d
Shyam4849 <piyush.soni8877@gmail.com>
2026-03-13 00:05:03 +0530

    LeetCode Problem 1

--- Arrays/two_sum.cpp
+++ Arrays/two_sum.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 vector<int> twoSum(vector<int> &nums, int target)
 {
+    // Brute Force
+    /*
     int n = nums.size();
     for (int i = 0; i < n; i++)
     {
@@ -15,12 +17,63 @@ vector<int> twoSum(vector<int> &nums, int target)
         }
     }
     return {-1, -1};
+    */
+
+    // Optimal Approach -> when not sorted but best for general cases
+    /*
+    unordered_map<int, int> sum;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        int complement = target - nums[i];
+
+        if (sum.find(complement) != sum.end())
+        {
+            return {sum[complement], i};
+        }
+        sum[nums[i]] = i;
+    }
+    return {-1, -1};
+    */
+
+    // Optimal Approach -> when already sorted
+    int n = nums.size();
+
+    vector<pair<int, int>> originalIndex;
+    for (int i = 0; i < n; i++)
+    {
+        originalIndex.push_back({nums[i], i});
+    }
+
+    sort(originalIndex.begin(), originalIndex.end());
+
+    int i = 0;
+    int j = n - 1;
+
+    while (i < j)
+    {
+        int sum = originalIndex[i].first + originalIndex[j].first;
+        if (sum == target)
+        {
+            return {originalIndex[i].second, originalIndex[j].second};
+        }
+        else if (sum > target)
+        {
+            j--;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    return {-1, -1};
 }
 
 int main()
 {
-    vector<int> nums = {2, 7, 11, 15};
-    int target = 18;
+    vector<int> nums = {3, 2, 4};
+    int target = 6;
     vector<int> result = twoSum(nums, target);
     for (auto &val : result)
         cout << val << " ";
