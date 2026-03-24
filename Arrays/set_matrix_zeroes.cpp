5a1bafe939df71090ce390c5e4f51f6df4339372
Shyam4849 <piyush.soni8877@gmail.com>
2026-03-24 23:08:13 +0530

    LeetCode Problem 73 - Brute Force

---
+++ Arrays/set_matrix_zeroes.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void setZeroes(vector<vector<int>> &matrix)
+{
+    int r = matrix.size();    // storing the number of rows
+    int c = matrix[0].size(); // storing the number of columns
+
+    vector<pair<int, int>> zeroPos;
+
+    // Store all the positions of Zeroes
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                zeroPos.push_back({i, j});
+            }
+        }
+    }
+
+    // Replacing the row element and column element with zero
+    for (auto x : zeroPos)
+    {
+        int row = x.first;
+        int col = x.second;
+
+        // Replacing the rows with zero
+        for (int j = 0; j < c; j++)
+        {
+            matrix[row][j] = 0;
+        }
+        // Replacing the columns with zero
+        for (int i = 0; i < r; i++)
+        {
+            matrix[i][col] = 0;
+        }
+    }
+
+    // Printing the matrix
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    setZeroes(matrix);
+}
