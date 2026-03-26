dbe1128e963e0534c2e346e3e7855a68cb7653b9
Shyam4849 <piyush.soni8877@gmail.com>
2026-03-26 20:14:23 +0530

    LeetCode Problem 73

--- Arrays/set_matrix_zeroes.cpp
+++ Arrays/set_matrix_zeroes.cpp
@@ -52,9 +52,10 @@ void setZeroes(vector<vector<int>> &matrix)
     */
 
     // Better Approach
-
+    /*
     int r = matrix.size();    // storing the number of rows
     int c = matrix[0].size(); // storing the number of columns
+
     vector<bool> row(r, false);
     vector<bool> col(c, false);
 
@@ -77,7 +78,79 @@ void setZeroes(vector<vector<int>> &matrix)
         for (int j = 0; j < c; j++)
         {
             if (row[i] == true || col[j] == true)
+            matrix[i][j] = 0;
+        }
+    }
+    */
+
+    // Optimal Approach
+
+    int r = matrix.size();    // Storing the number of rows
+    int c = matrix[0].size(); // Storing the number of columns
+
+    bool rowImpacted = false;
+    bool colImpacted = false;
+
+    // Checking the row is impacted or not
+    for (int col = 0; col < c; col++)
+    {
+        if (matrix[0][col] == 0)
+        {
+            rowImpacted = true;
+            break;
+        }
+    }
+
+    // Checking the column is impacted or not
+    for (int row = 0; row < r; row++)
+    {
+        if (matrix[row][0] == 0)
+        {
+            colImpacted = true;
+            break;
+        }
+    }
+
+    // Setting marker of first row and column
+    for (int i = 0; i < r; i++)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            if (matrix[i][j] == 0)
+            {
+                matrix[0][j] = 0;
+                matrix[i][0] = 0;
+            }
+        }
+    }
+
+    // Corner elements
+    for (int i = 1; i < r; i++)
+    {
+        for (int j = 1; j < c; j++)
+        {
+            if (matrix[0][j] == 0 || matrix[i][0] == 0)
+            {
                 matrix[i][j] = 0;
+            }
+        }
+    }
+
+    // Replacing the row with zero
+    if (rowImpacted)
+    {
+        for (int j = 0; j < c; j++)
+        {
+            matrix[0][j] = 0;
+        }
+    }
+
+    // Replacing the column with zero
+    if (colImpacted)
+    {
+        for (int i = 0; i < r; i++)
+        {
+            matrix[i][0] = 0;
         }
     }
 
