29be34ed83e17f6d89f441a918b4bb809e8bc68b
Shyam4849 <piyush.soni8877@gmail.com>
2026-04-03 21:08:28 +0530

    LeetCode Problem 118

---
+++ Arrays/pascals_triangle.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> generate(int numRows)
+{
+    vector<vector<int>> result;
+
+    for (int r = 0; r < numRows; r++)
+    {
+        vector<int> row(r + 1, 1);
+
+        for (int c = 1; c < r; c++)
+        {
+            row[c] = result[r - 1][c - 1] + result[r - 1][c];
+        }
+        result.push_back(row);
+    }
+
+    return result;
+}
+
+int main()
+{
+    int numRows = 6;
+    vector<vector<int>> result = generate(numRows);
+    for (int i = 0; i < result.size(); i++)
+    {
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            cout << result[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
