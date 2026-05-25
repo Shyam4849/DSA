285b306b052ca7da6229111c6064a7de35deeb31
Shyam4849 <piyush.soni8877@gmail.com>
2026-05-25 22:36:35 +0530

    A2Z Striver - Better Approach

---
+++ Arrays/repeating_and_missing_number.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> missingAndRepeatingNumber(vector<int> nums)
+{
+    int n = nums.size();
+    int repeating = -1, missing = -1;
+
+    // Brute Force
+    /*
+    for (int i = 1; i <= n; i++)
+    {
+        int count = 0;
+        for (int j = 0; j < n; j++)
+        {
+            if (nums[j] == i)
+            count++;
+        }
+
+        if (count == 2)
+        repeating = i;
+        else if (count == 0)
+        missing = i;
+
+        if (repeating != -1 && missing != -1)
+        break;
+    }
+    */
+
+    // Better Approach
+    int hash[n + 1] = {0};
+
+    for (int i = 0; i < n; i++)
+    {
+        hash[nums[i]]++;
+    }
+
+    for (int i = 0; i <= n; i++)
+    {
+        if (hash[i] == 2)
+            repeating = i;
+        else if (hash[i] == 0)
+            missing = i;
+
+        if (repeating != -1 && missing != -1)
+            break;
+    }
+
+    return {repeating, missing};
+}
+
+int main()
+{
+    vector<int> nums = {1, 2, 3, 6, 7, 5, 7};
+    vector<int> result = missingAndRepeatingNumber(nums);
+
+    cout << result[0] << " " << result[1] << endl;
+    cout << "Repeating number is: " << result[0] << " and Missing Number is: " << result[1];
+}
