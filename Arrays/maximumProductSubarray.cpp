9f9bd69f7052011d11337d26f8f2ee51a972451b
Shyam4849 <piyush.soni8877@gmail.com>
2026-06-01 00:32:31 +0530

    LeetCode Probem 152

--- Arrays/maximumProductSubarray.cpp
+++ Arrays/maximumProductSubarray.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 int maxProduct(vector<int> &nums)
 {
+    // Brute Force
+    /*
     int n = nums.size();
     int result = nums[0];
 
@@ -16,6 +18,33 @@ int maxProduct(vector<int> &nums)
         }
     }
     return result;
+    */
+
+    // Optimal Approach
+    int n = nums.size();
+
+    if (n == 0)
+        return 0;
+
+    int global_max = nums[0];
+    int curr_max = nums[0];
+    int curr_min = nums[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        int curr_num = nums[i];
+
+        if (curr_num < 0)
+            swap(curr_max, curr_min);
+
+        int temp_max = max(curr_num, curr_num * curr_max);
+        curr_min = min(curr_num, curr_num * curr_min);
+
+        curr_max = temp_max;
+
+        global_max = max(global_max, curr_max);
+    }
+    return global_max;
 }
 
 int main()
